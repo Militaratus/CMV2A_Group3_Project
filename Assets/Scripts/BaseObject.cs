8a7e4f8bdc92d3feacc57a7aac9997cda8b462f0
Militaratus <jarednealon@outlook.com>
2017-10-02 10:05:25 +0200

    Initial Project Commit

---
+++ Assets/Scripts/BaseObject.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseObject : MonoBehaviour
+{
+    Material startingMaterial;
+    public Material selectMaterial;
+
+    Renderer rend;
+
+    bool amActive = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+        startingMaterial = rend.material;
+        
+    }
+
+    private void Update()
+    {
+        if (amActive && Input.GetAxis("Fire1") >= 1)
+        {
+            ObjectAction();
+        }
+
+        if (Input.GetAxis("Fire2") >= 1)
+        {
+            ObjectHide();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        if (amActive)
+        {
+            amActive = false;
+        }
+        else
+        {
+            rend.material = startingMaterial;
+        }
+    }
+
+    public void ObjectHit()
+    {
+        amActive = true;
+        rend.material = selectMaterial;
+    }
+
+    public virtual void ObjectAction()
+    {
+        print("Main Screen Turn On");
+    }
+
+    public virtual void ObjectHide()
+    {
+
+    }
+}
---
+++ Assets/Scripts/LaptopObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaptopObject : BaseObject
+{
+    public GameObject mainScreen;
+
+    public override void ObjectAction()
+    {
+        base.ObjectAction();
+
+        if (mainScreen)
+        {
+            mainScreen.SetActive(true);
+        }
+    }
+
+    public override void ObjectHide()
+    {
+        base.ObjectHide();
+
+        mainScreen.SetActive(false);
+    }
+}
